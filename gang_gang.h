db1df74fd7525ff88ecdb74cfea3f8629e267eb6
kezyui | juantwo <114587600+FortniteEnjoyer65@users.noreply.github.com>
2023-02-19 05:36:58 +1000

    Update gang_gang.h

--- gang_gang.h
+++ gang_gang.h
@@ -1,7 +1,8 @@
 /*
-  StaticFindObject: 0x4777024
-  GetBoneMatrix: 0x47AD41C
-  ProcessEvent: 0x0D15B40 | 0x4C
+  UObject::StaticFindObject ->0x4777024
+  USkinnedMeshComponent::GetBoneMatrix -> 0x47AD41C
+  UObject::ProcessEvent -> 0x0D15B40 | 0x4C
+  ULocalPlayer::GetViewPoint -> 0x0F046CC | 0x2B8
   
   19/02/23
 */
