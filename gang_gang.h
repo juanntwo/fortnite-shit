a20ffa23d0db2f7008d5be5b7a49b922f4bdd89b
kez <114587600+juanntwo@users.noreply.github.com>
2023-03-03 02:32:34 +1000

    Update gang_gang.h

--- gang_gang.h
+++ gang_gang.h
@@ -1,8 +1,6 @@
 /*
-  UObject::StaticFindObject ->0x4777024
-  USkinnedMeshComponent::GetBoneMatrix -> 0x47AD41C
-  UObject::ProcessEvent -> 0x0D15B40 | 0x4C
-  ULocalPlayer::GetViewPoint -> 0x0F046CC | 0x2B8
-  
+  UObject::StaticFindObject -> 0x4795C44
+  FName::ToString -> 0x478F044
+  UObject::ProcessEvent -> 0x4C
   19/02/23
 */
